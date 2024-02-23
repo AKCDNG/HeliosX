fb1019bdf9adb52728288acbae25102fe670a1aa
Anish <anishkakaiya@outlook.com>
2024-02-23 10:56:17 +0000

    add hook to track answers dependancy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.scss';
+import Header from './Header';
 
 const Questionnaire = () => {
   const questions: Array<string> = [
@@ -13,15 +14,18 @@ const Questionnaire = () => {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0);
   const [answers, setAnswers] = useState<Array<string>>([]);
 
+  useEffect(() => {
+    if (answers.length === questions.length) {
+      sendDataToAPI(answers);
+      console.log(currentQuestionIndex);
+    }
+  }, [answers]);
+
   const handleAnswer = (answer: string) => {
     setAnswers((prevAnswers: Array<string>) => [...prevAnswers, answer]);
 
-    if (currentQuestionIndex < questions.length - 1) {
+    if (currentQuestionIndex < questions.length) {
       setCurrentQuestionIndex((prevIndex: number) => prevIndex + 1);
-    } else {
-      console.log('Thank you for completing the questionnaire!');
-
-      sendDataToAPI(answers);
     }
   };
 
@@ -30,16 +34,32 @@ const Questionnaire = () => {
   };
 
   return (
-    <section className='col-12 col-lg-6'>
-      <div className='header' key={currentQuestionIndex}>
-        <h2>Question {[currentQuestionIndex + 1]}:</h2>
-        <h3>{questions[currentQuestionIndex]}</h3>
-      </div>
-      <div className='buttons'>
-        <button onClick={() => handleAnswer('Yes')}>Yes</button>
-        <button onClick={() => handleAnswer('No')}>No</button>
-      </div>
-    </section>
+    <div className='wrapper'>
+      <Header />
+      <section className='container'>
+        {currentQuestionIndex < 5 ? (
+          <div className='question-wrap'>
+            <div className='row'>
+              <div className='header col-12' key={currentQuestionIndex}>
+                <h2>Question {[currentQuestionIndex + 1]}:</h2>
+                <h3>{questions[currentQuestionIndex]}</h3>
+              </div>
+            </div>
+            <div className='row'>
+              <div className='buttons col-12'>
+                <button onClick={() => handleAnswer('Yes')}>Yes</button>
+                <button onClick={() => handleAnswer('No')}>No</button>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className='thank-you-msg'>
+            <h2>Thank you for your submission</h2>
+            <h3>We will be in touch soon.</h3>
+          </div>
+        )}
+      </section>
+    </div>
   );
 };
 
