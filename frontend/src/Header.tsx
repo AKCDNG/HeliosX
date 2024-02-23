f0bfa173c15a2ff4a699c90a64a9078faaed7b58
Anish <anishkakaiya@outlook.com>
2024-02-23 00:45:52 +0000

    add basic header and styling

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className='col-12'>
+      <img src='src/assets/dermatica-logo.png' alt='' height={40} />
+    </header>
+  );
+};
+
+export default Header;
